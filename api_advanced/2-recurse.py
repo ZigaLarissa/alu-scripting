f5113f984a91ec4c8ee9e230b78e0c11b3540daa
B.Larissa <109439888+ZigaLarissa@users.noreply.github.com>
2023-03-07 16:08:10 +0200

    Recurse it!

--- api_advanced/2-recurse.py
+++ api_advanced/2-recurse.py
@@ -11,27 +11,26 @@ import requests
 def recurse(subreddit, hot_list=[], after=None):
     url = 'https://www.reddit.com/r/{}/hot.json'.format(subreddit)
     headers = {'User-Agent': 'Mozilla/5.0'}
-    params = {'after': after} if after else {} #After is set to none at first.
-
+    params = {'after': after} if after else {}  # After is set to none at first.
 
     response = requests.get(url, headers=headers, params=params)
 
     if response.status_code == 200:
         data = response.json()
-        
+  
         for post in data['data']['children']:
             title = post['data']['title']
             hot_list.append(title)
-        
-         #set after to the last post of the previous result.
+
+        # set after to the last post of the previous result.
         after = data['data']['after']
-        
-         #call the function with the updated after i.e recursion
-         # to retrieve addition pages.
+
+        # call the function with the updated after i.e recursion
+        # to retrieve addition pages.
         if after:
             recurse(subreddit, hot_list, after)
-    
+
     else:
         return None
-    
+
     return hot_list
